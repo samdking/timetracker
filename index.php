29d3cd5f638fb9e4f18d959cf627cb9c37fe2b74
Sam King <sdking02@gmail.com>
2012-05-17 00:12:01 +0100

    Counts up and down in secs; added pause functionality

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	<script src="timetracker.js"></script>
 	<link rel="stylesheet" href="timetracker.css"></link>
+	<title>Bozboz Timetracker</title>
 </head>
 <body>
 
@@ -14,7 +15,8 @@
 			<fieldset>
 				<input name="value" type="text">
 				<div id="suggestions"></div>
-				<input type="submit" value="Start">
+				<input type="submit" id="startstop" value="Start">
+				<input type="button" id="pause" value="Pause">
 			</fieldset>
 		</form>
 
--- timetracker.js
+++ timetracker.js
@@ -1,11 +1,15 @@
 $(function() {
 	
-	var $suggestions = $('#suggestions');
-	var $field = $('#timetracker input[type=text]');
-	var $form = $('#timetracker');
-	var $time = $('#time');
+	var $suggestions = $('#suggestions'),
+	    $field = $('#timetracker input[type=text]'),
+	    $form = $('#timetracker'),
+	    $pause = $('#timetracker #pause'),
+	    $time = $('#time');
 
-	var timing = false;
+	var timing = false,
+	    timer,
+	    start_time,
+	    paused_time;
 
 	$form.on('submit', function() {
 		if ($field.hasClass('valid') == false)
@@ -14,7 +18,27 @@ $(function() {
 		$form.find('input[type=submit]').val((timing? 'Start' : 'Stop'));
 		$time.removeClass('inactive');
 		timing = !timing;
+		start_time = new Date().getTime();
+		timer = window.setInterval(function() {
+			$time.html(timeIt(start_time));
+		}, 1000);
 		return false;
+	});
+
+	$pause.click(function() {
+		if (timing) {
+			paused_time = new Date().getTime();
+			window.clearInterval(timer);
+			$time.addClass('paused');
+			timing = false;
+		} else {
+			start_time += (new Date().getTime() - paused_time);
+			timer = window.setInterval(function() {
+				$time.html(timeIt(start_time));
+			}, 1000);
+			$time.removeClass('paused');
+			timing = true;
+		}
 	});
 
 	$field.on('keyup', function(e) {
@@ -33,3 +57,11 @@ $(function() {
 	});
 
 });
+
+function timeIt(start_time) {
+	var time = new Date().getTime() - start_time,
+	    total = Math.floor(time / 1000),
+	    mins = Math.floor(total / 60),
+	    secs = total%60;
+	return mins + ':' + (secs < 10? '0' : '') + secs;
+}

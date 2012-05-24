0208c8afbede9347b2699279fe7d6531a67544af
Sam King <sdking02@gmail.com>
2012-05-25 00:17:38 +0100

    Included functions file, added exit if no config file exists

--- request.php
+++ request.php
@@ -3,8 +3,13 @@
 include 'system/sql_classes.php';
 include 'system/query_set.php';
 include 'system/model.php';
-include 'system/config.php';
 include 'system/engine.php';
+include 'system/functions.php';
+
+if (!file_exists('system/config.php'))
+	error('No config file exists');
+
+include 'system/config.php';
 
 $action = isset($_GET['action'])? $_GET['action'] : 'query';
 

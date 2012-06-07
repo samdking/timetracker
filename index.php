a71638a658a986359dfa9483e0544b7029b0d8e5
Sam King <sdking02@gmail.com>
2012-06-07 17:49:19 +0100

    Formatting whitespace

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-	include 'system/init.php';
-	$me = $_GET['me'];
-	$time = Model::get('time')->filter(array('user_id'=>$me, 'finished'=>0))->order('paused ASC')->one();
-	if ($time) {
-		$client = $time->client();
-		$start_time = strtotime($time->start_time) * 1000;
-		$offset = $time->paused_time();
-	}
-	$active_times = Model::get('time')->filter(array('finished'=>0, 'user_id'=>$me));
-	include 'app/views/index.php';
-?>
+
+include 'system/init.php';
+$me = $_GET['me'];
+$time = Model::get('time')->filter(array('user_id'=>$me, 'finished'=>0))->order('paused ASC')->one();
+if ($time) {
+	$client = $time->client();
+	$start_time = strtotime($time->start_time) * 1000;
+	$offset = $time->paused_time();
+}
+$active_times = Model::get('time')->filter(array('finished'=>0, 'user_id'=>$me));
+include 'app/views/index.php';

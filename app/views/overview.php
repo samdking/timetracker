d254994c42f78d44d41bcb6a9662af5554e8a29a
Sam King <sdking02@gmail.com>
2012-06-11 16:24:53 +0100

    Cleaning up overview view - getting rid of old table code

--- app/views/overview.php
+++ app/views/overview.php
@@ -1,22 +1,3 @@
-<!--<table id="overview-table">
-	<thead>
-		<tr>
-			<th>Client</th>
-			<th>Start time</th>
-			<th>Duration</th>
-		</tr>
-	</thead>
-	<tbody>
-<?php foreach($times as $time): ?>
-		<tr>
-			<td><?=$time->client()->name?></td>
-			<td><?=date('h:i A', strtotime($time->start_time))?></td>
-			<td><?=format_mins($time->total_mins)?></td>
-		</tr>
-<?php endforeach ?>
-	</tbody>
-</table>
-<input type="button" id="send" class="btn" value="Send">-->
 	<input type="hidden" name="date" value="<?=date('Y-m-d')?>">
     <input type="hidden" name="me" value="<?=$me?>">
     <input type="hidden" name="agent" value="api">

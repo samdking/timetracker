8124666c98017496f302fd162c025754c38c5b19
Sam King <sdking02@gmail.com>
2012-06-15 01:12:24 +0100

    Amended SQL dump - thinning out times table

--- sql_dump.sql
+++ sql_dump.sql
@@ -14,14 +14,9 @@ DROP TABLE IF EXISTS `times`;
 
 CREATE TABLE `times` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `client_id` int(11) DEFAULT NULL,
-  `start_time` datetime DEFAULT NULL,
-  `paused` tinyint(1) DEFAULT NULL,
-  `paused_time` datetime DEFAULT NULL,
-  `secs_paused` int(11) DEFAULT NULL,
+  `client_id` int(11) DEFAULT NULL,L,
   `log_message` text,
   `total_mins` int(11) NOT NULL,
-  `finished` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
